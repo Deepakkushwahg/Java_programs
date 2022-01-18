097433feeb36e474cd16c74f6e48e9eaf525451a
Deepakkushwahg <deepakkushwahg@gmail.com>
2022-01-18 17:31:28 +0530

    modify

--- Multiplication_table.java
+++ Multiplication_table.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class Multiplication_table 
 {
-    public static void main(String[] args) 
+    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        int i,n;
         System.out.print("Enter the number: ");
-        n = sc.nextInt();
-        for(i=1;i<=10;i++)
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++)
         {
-            System.out.println(n+" x "+i+" = "+n*i);
+            System.out.println(n+" x "+i+"\t=\t"+n*i);
         }
 
         
