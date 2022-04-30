26e979dae249d763f6e20921e9b0ade83c307a9a
Deepakkushwahg <deepakkushwahg@gmail.com>
2022-04-30 20:28:13 +0530

    New commit

---
+++ Data_structure_and_algorithm/Tree_data_structure/built_preorder_tree.java
@@ -0,0 +1,107 @@
+package Data_structure_and_algorithm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class built_preorder_tree
+{
+    static int a=1,flag=0;
+    static Node root;
+    static class Node
+    {
+        Node left,right;
+        int data;
+        Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+    static void preorder(Node NewNode,int label)
+    {
+        if(Math.pow(2,label)-3<=0)
+            flag=1;
+        if(Math.pow(2,label)-3>0)
+        {
+            label--;
+            if(flag==0)
+                NewNode.left = new Node(++a);
+            else
+                NewNode.right = new Node(++a);
+            preorder(NewNode.left,label);
+            preorder(NewNode.right,label);
+        }
+    }
+    static void PrintByPreorder(Node root)
+    {
+        if(root==null)
+            return;
+        System.out.print(root.data+" ");
+        PrintByPreorder(root.left);
+        PrintByPreorder(root.right);
+    }
+    static ArrayList<Integer> ls = new ArrayList<Integer>();
+    void leftNodes(Node node)
+    {
+        if(node.left!=null)
+        {
+            ls.add(node.data);
+            leftNodes(node.left);
+
+        }
+        else if(node.right!=null)
+        {
+            ls.add(node.data);
+            leftNodes(node.right);
+        }
+    }
+    void leafNodes(Node node)
+    {
+        if(node.left==null && node.right==null)
+        {
+            ls.add(node.data);
+            return;
+        }
+        else if(node!=null && node.left!=null)
+        {
+            leafNodes(node.left);
+        }
+        if(node.right!=null)
+            leafNodes(node.right);
+    }
+    void rightNodes(Node node)
+    {
+        if(node.right!=null)
+        {
+            rightNodes(node.right);
+            ls.add(node.data);
+        }
+        else if(node.left!=null)
+        {
+            ls.add(node.data);
+            rightNodes(node.left);
+        }
+    }
+    public static void main(String[] args) {
+//        System.out.print("Enter the label of tree: ");
+//        int label = new Scanner(System.in).nextInt();
+//        root = new Node(1);
+//        preorder(root,label);
+//        PrintByPreorder(root);
+        root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(9);
+        root.left.left.left = new Node(6);
+        root.left.left.right = new Node(5);
+        root.left.right.right = new Node(3);
+        root.left.right.right.left = new Node(7);
+        root.left.right.right.right = new Node(8);
+        built_preorder_tree obj = new built_preorder_tree();
+        obj.leftNodes(root);
+        obj.leafNodes(root);
+        if(root.right!=null)
+            obj.rightNodes(root.right);
+        System.out.println(ls);
+    }
+}
---
+++ Data_structure_and_algorithm/Tree_data_structure/deletion_in_tree.java
@@ -0,0 +1,87 @@
+package Data_structure_and_algorithm;
+
+// implementation of insertion according to sorted element and deletion of element from that tree
+import java.util.Scanner;
+
+public class deletion_in_tree
+{
+
+    static Node root;
+    static class Node
+    {
+        Node left,right;
+        int data;
+        Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+    static Node Insertion(Node parentNode,int x)
+    {
+        if(parentNode==null)
+        {
+            Node newNode = new Node(x);
+            return newNode;
+        }
+        if(parentNode.data>x)
+            parentNode.left = Insertion(parentNode.left,x);
+        else
+        {
+            parentNode.right = Insertion(parentNode.right,x);
+        }
+        return parentNode;
+    }
+    static int Min(Node root)
+    {
+        if(root.left==null)
+            return root.data;
+        return Min(root.left);
+    }
+    static Node deletion(Node currentNode,int x)
+    {
+        if(currentNode==null)
+            return null;
+        if(x<currentNode.data)
+            currentNode.left = deletion(currentNode.left,x);
+        else if(x>currentNode.data)
+            currentNode.right = deletion(currentNode.right,x);
+        else
+        {
+            if(currentNode.left==null)
+                return currentNode.right;
+            else if(currentNode.right==null)
+                return currentNode.left;
+            else
+            {
+                currentNode.data = Min(currentNode.right);
+                currentNode.right = deletion(currentNode.right,currentNode.data);
+            }
+        }
+        return currentNode;
+    }
+    static void PrintInOrder(Node root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        PrintInOrder(root.left);
+        System.out.print(root.data+" ");
+        PrintInOrder(root.right);
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter number of elements: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the elements");
+        for(int i=0;i<n;i++)
+        {
+            root = Insertion(root,sc.nextInt());
+        }
+        System.out.print("Enter the element which you want to delete from tree: ");
+        int value = sc.nextInt();
+        root = deletion(root,value);
+        PrintInOrder(root);
+    }
+}
