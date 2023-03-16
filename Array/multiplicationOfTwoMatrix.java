f94c259a0915a963ddf69400472852a57d97bd5c
Deepakkushwahg <deepakkushwahg@gmail.com>
2023-03-16 14:41:41 +0530

    multiplicationOfTwoMatrix

--- Array/multiplicationOfTwoMatrix.java
+++ Array/multiplicationOfTwoMatrix.java
@@ -25,18 +25,21 @@ public class multiplicationOfTwoMatrix {
                 B[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Multiplication of two matrix is");
-        int s=0;
-        int[][] C = new int[rowA][columnB];
-        for(int i=0;i<rowA;i++){
-            for(int j=0;j<columnB;j++){
-                for(int l=0,k=0;l<columnA && k<rowB;l++,k++)
-                    s=s+A[i][l]*B[k][j];
-                C[i][j]=s;
-                System.out.print(C[i][j]+" ");
-                s=0;
+        if(columnA!=rowB) System.out.println("Multiplication is not possible, no. of column in matrix A and no. of row in matrix B must be same");
+        else{
+            System.out.println("Multiplication of two matrix is");
+            int s=0;
+            int[][] C = new int[rowA][columnB];
+            for(int i=0;i<rowA;i++){
+                for(int j=0;j<columnB;j++){
+                    for(int l=0;l<columnA;l++)
+                        s=s+A[i][l]*B[l][j];
+                    C[i][j]=s;
+                    System.out.print(C[i][j]+" ");
+                    s=0;
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
